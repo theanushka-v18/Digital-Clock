2cb92335a07c6c0e6be6389b09b664001cc32369
Anushka Verma <anushkaverma779@gmail.com>
2022-11-11 14:01:59 +0530

    Completed the code of this project

---
+++ script.js
@@ -0,0 +1,26 @@
+function currTime() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+
+    if(hh == 0) {
+        hh = 12;
+    }
+    if(hh > 12) {
+        hh = hh - 12;
+        session = "PM";
+    }
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + ":" + ss + " " + session;
+    document.getElementById("clock").innerText = time;
+
+    let t = setTimeout(function() {
+        currTime();
+    }, 1000);
+}
+currTime();
